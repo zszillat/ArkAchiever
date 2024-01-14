fbc72735093cb3dad1cbabfeeb18e2859bd99347
psyfarkledon <88149712+psyfarkledon@users.noreply.github.com>
2024-01-14 12:58:52 -0500

    NewAchievementActivityPageTwo.java now sets whatever the achievement name from the previous activity.

--- app/src/main/java/com/apps/arkachiever3/activities/new_achievement/NewAchievementActivityPageOne.java
+++ app/src/main/java/com/apps/arkachiever3/activities/new_achievement/NewAchievementActivityPageOne.java
@@ -1,4 +1,4 @@
-package com.apps.arkachiever3;
+package com.apps.arkachiever3.activities.new_achievement;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.apps.arkachiever3.R;
 
 public class NewAchievementActivityPageOne extends AppCompatActivity {
 
--- app/src/main/java/com/apps/arkachiever3/activities/new_achievement/NewAchievementActivityPageTwo.java
+++ app/src/main/java/com/apps/arkachiever3/activities/new_achievement/NewAchievementActivityPageTwo.java
@@ -1,12 +1,16 @@
-package com.apps.arkachiever3;
+package com.apps.arkachiever3.activities.new_achievement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.apps.arkachiever3.R;
 
 public class NewAchievementActivityPageTwo extends AppCompatActivity {
 
+    TextView textViewNewAchievement;
     String achievementName;
 
     @Override
@@ -17,6 +21,11 @@ public class NewAchievementActivityPageTwo extends AppCompatActivity {
         //Intent
         Intent intent = getIntent();
         achievementName = intent.getStringExtra("name");
+
+        //Associate Components
+        textViewNewAchievement = this.findViewById(R.id.textViewNewAchievement);
+
+        textViewNewAchievement.setText(achievementName);
 
     }
 }
